21c325b8dee699bbb9534533dc6d4f4c7f98295e
SashaM91 <113477842+SashaM91@users.noreply.github.com>
2023-12-04 01:16:09 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const modalElement = document.querySelector('.modal-oddzwon'); 
-const formElement = document.querySelector('.modal'); 
-const orderButtons = document.querySelectorAll('.action-button'); 
-const closeButton = document.querySelector('.close-modal'); 
+const modalElementKoszt = document.querySelector('.modal-obliczyć-koszt'); 
+const formElement = document.querySelector('.modal');  
+const orderButtons = document.querySelectorAll('.action-button');  
+const orderButtonsKoszt = document.querySelectorAll('.action-button-koszt')
+const closeButton = document.querySelector('.close-modal');
+const orderButtonObliczyc = document.querySelector('.action-button-obliczyć');
+const modalDziękuję = document.querySelector('.modal-Dziękuję-za-twoją-prośbę');
+
 const onClick = () => { 
     modalElement.classList.add('shown'); 
 }; 
+const onClickKoszt = () => { 
+    modalElementKoszt.classList.add('shown'); 
+
+}; 
+const onClickDziękuję = () => { 
+    modalDziękuję.classList.add('shown'); 
+
+}; 
+
 const onCloseClick = () => { 
     modalElement.classList.remove('shown'); 
 }; 
@@ -13,4 +27,16 @@ orderButtons.forEach((orderButton) => {
     orderButton.addEventListener('click', onClick); 
 }); 
  
-closeButton.addEventListener('click', onCloseClick);  
+orderButtonsKoszt.forEach((orderButton) =>{ 
+    orderButton.addEventListener('click', onClickKoszt); 
+});
+
+orderButtonObliczyc.forEach((orderButton) =>{ 
+    orderButton.addEventListener('click', onClickDziękuję); 
+});
+
+
+closeButton.addEventListener('click', onCloseClick); 
+
+
+
